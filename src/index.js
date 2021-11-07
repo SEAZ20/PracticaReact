6c041df9a78ef753a3d0decbcf0a40a8191c63ba
SEAZ20 <seaz199820@gmail.com>
2021-11-07 15:58:33 -0500

    Components and styles

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
 import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
-
-const user={
-  firstName: 'Silvio',
-  lastName:'Alcivar',
-  avatar: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png'
-}
-function getName(user){
-  return `${user.firstName} ${user.lastName}`
-}
-
-function getGreeting(user) {
-  if (user) {
-    return ( 
-    <div>
-      <h1>Hello, {getName(user)}!</h1>;
-      <img src={user.avatar}/>
-    </div>)
-   
-  }
-  return <h1>Hello, Stranger.</h1>;
-}
-const name= "Silvio "
-
-const element= <div>{getGreeting(user)}</div>
+import Card from './components/Card';
+import 'bootstrap/dist/css/bootstrap.css'
 const container= document.getElementById('root');
-ReactDOM.render(
-element,container
-);
+
+ReactDOM.render(<Card/>,container);
 
